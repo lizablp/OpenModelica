cdf5e5905a19166b4ce23b8b59801ae74d60d0c6
Lizab <lizabp@hotmail.fr>
2023-05-16 16:51:16 +0200

    add OMPython

--- OMPython/ompython.py
+++ OMPython/ompython.py
@@ -1,13 +1,55 @@
 import OMPython
 from OMPython import OMCSessionZMQ
-#omc=OMPython.OMCSession() # error with the command : locale.Error: unsupported locale setting OMC did not exit after being sent the quit() command; killing the process with pid=18336
-model_file ='HeatingSystemfonctionall.mo'
+import zipfile
+import subprocess
+import os
+model_file ='HeatingSystemfonctional.mo'
 omc = OMCSessionZMQ()
 cmds = [
-  'loadFile("HeatingSystemfonctional.mo")',
+  'loadFile(getInstallationDirectoryPath() + "/share/doc/omc/testmodels/HeatingSystemfonctional.mo")',
   "simulate(HeatingSystemfonctional)",
   "plot(h)"
   ]
 for cmd in cmds:
   answer = omc.sendExpression(cmd)
   print("\n{}:\n{}".format(cmd, answer))
+
+zip_file_path = "C:\\Users\\Lizab\\AppData\\Local\\Temp\\OpenModelica\\OMEdit\\HeatingSystemfonctional\\HeatingSystemfonctional.fmu" #path to change
+extract_path = "C:\\Users\\Lizab\\AppData\\Local\\Temp\\OpenModelica\\OMEdit\\HeatingSystemfonctional\\HeatingSystemfonctional_FMU" #Directory extracted
+
+with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+    # Extract all the contents to the specified directory
+    zip_ref.extractall(extract_path)
+
+print("Extraction completed.")
+
+source_dir = 'C:/Users/Lizab/AppData/Local/Temp/OpenModelica/OMEdit/HeatingSystemfonctional/HeatingSystemfonctional_FMU/sources'
+build_dir = 'build_cmake'
+
+os.chdir(source_dir)
+command = [
+    'cmake',
+    '-S',
+    '.',
+    '-B',
+    'build_cmake',
+    '-D',
+    'RUNTIME_DEPENDENCIES_LEVEL=modelica',
+    '-D',
+    'CMAKE_C_COMPILER=clang',
+    '-D',
+    'CMAKE_CXX_COMPILER=clang++',
+    '-D',
+    'FMI_INTERFACE_HEADER_FILES_DIRECTORY=C:\\Programmes\\OpenModelica\\include\\omc\\c\\fmi'
+]
+
+'''subprocess.run(command)
+subprocess.run(["cmake", "-E", "make_directory", build_dir], check=True)
+subprocess.run(["cmake", source_dir], cwd=build_dir, check=True)
+subprocess.run(["cmake", "--build", "."], cwd=build_dir, check=True)'''
+
+modelica_script = """
+list(OpenModelica.Scripting.importFMU, interfaceOnly=true);
+"""
+result = omc.execute(modelica_script)
+print(result)
